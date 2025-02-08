692d0522e86a546083a53585cd8296ad1c295fdf
Rajesh <rajeshoffice07@gmail.com>
2025-02-08 10:26:46 +0530

    add module for clear cors error in next.config file

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
+  module : {
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "4mb",
@@ -14,6 +31,7 @@ const nextConfig: NextConfig = {
       }
     ]
   }
+  
 };
 
 export default nextConfig;
